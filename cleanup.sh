064488e915928d0f3aeaef1fac0a090c1e0c5934
mikhail <66185525+mikhailuwu@users.noreply.github.com>
2024-09-08 19:12:14 +0000

    modified:   cleanup.sh 	modified:   compile.sh 	modified:   make.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,3 @@
 rm -v -rf .ninja_deps .ninja_log build.ninja
 rm -v -rf CMakeFiles CMakeCache.txt cmake_install.cmake
+rm -v -rf Makefile
--- make.sh
+++ make.sh
@@ -1,1 +1,1 @@
-cmake -G Ninja
+cmake -G "Unix Makefiles" # Ninja
